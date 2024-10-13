ae2353371389f2710b1f30b4d9f18e3a527c5b14
cedric <dabrao@gmail.com>
2024-10-13 11:30:37 +0200

    adding support for x86_64-freebsd.

--- common/include/cothread/config.h
+++ common/include/cothread/config.h
@@ -13,25 +13,33 @@
 /// @ingroup doxy_cothread_config
 /// @{
 #define COTHREAD_CC_ID_GCC			1	///< @brief	The GNU C compiler identifier.
+#define COTHREAD_CC_ID_CLANG		2	///< @brief	The LLVM Clang compiler identifier.
 
 #define COTHREAD_ARCH_ID_X86		1	///< @brief	The x86 architecture identifier.
 #define COTHREAD_ARCH_ID_X86_64		2	///< @brief	The x86_64 architecture identifier.
 
 #define COTHREAD_OS_ID_GNU_LINUX	1	///< @brief	The GNU/Linux operating system identifier.
+#define COTHREAD_OS_ID_FREEBSD		2	///< @brief	The FreeBSD operating system identifier.
 /// @}
 
 #if		(defined(__GNUC__) && !defined(__clang__) && !defined(__MINGW32__))
 	#define	COTHREAD_CC_ID	COTHREAD_CC_ID_GCC
+#elif	(defined(__clang__))
+	#define	COTHREAD_CC_ID	COTHREAD_CC_ID_CLANG
 #endif
 
 #if		((COTHREAD_CC_ID_GCC == COTHREAD_CC_ID) && defined(__i386__))
 	#define	COTHREAD_ARCH_ID	COTHREAD_ARCH_ID_X86
 #elif	((COTHREAD_CC_ID_GCC == COTHREAD_CC_ID) && defined(__x86_64__))
 	#define	COTHREAD_ARCH_ID	COTHREAD_ARCH_ID_X86_64
+#elif	((COTHREAD_CC_ID_CLANG == COTHREAD_CC_ID) && defined(__x86_64__))
+	#define	COTHREAD_ARCH_ID	COTHREAD_ARCH_ID_X86_64
 #endif
 
 #if		((COTHREAD_CC_ID_GCC == COTHREAD_CC_ID) && defined(__gnu_linux__))
 	#define	COTHREAD_OS_ID		COTHREAD_OS_ID_GNU_LINUX
+#elif	((COTHREAD_CC_ID_CLANG == COTHREAD_CC_ID) && defined(__FreeBSD__))
+	#define	COTHREAD_OS_ID		COTHREAD_OS_ID_FREEBSD
 #endif
 
 #if		(!defined(COTHREAD_CC_ID)	|| (0 == COTHREAD_CC_ID))
@@ -63,6 +71,14 @@
 		&& (COTHREAD_CC_ID_GCC			== COTHREAD_CC_ID)		\
 		&& (COTHREAD_ARCH_ID_X86_64		== COTHREAD_ARCH_ID)	\
 		&& (COTHREAD_OS_ID_GNU_LINUX	== COTHREAD_OS_ID)		\
+		)
+	#define COTHREAD_LINK_EXPORT
+	#define	COTHREAD_LINK_HIDDEN	__attribute__ ((visibility ("hidden")))
+	#define COTHREAD_CALL			__attribute__ ((sysv_abi))
+#elif	(!0	\
+		&& (COTHREAD_CC_ID_CLANG		== COTHREAD_CC_ID)		\
+		&& (COTHREAD_ARCH_ID_X86_64		== COTHREAD_ARCH_ID)	\
+		&& (COTHREAD_OS_ID_FREEBSD		== COTHREAD_OS_ID)		\
 		)
 	#define COTHREAD_LINK_EXPORT
 	#define	COTHREAD_LINK_HIDDEN	__attribute__ ((visibility ("hidden")))
--- cothreadj/unittest/src/main.c
+++ cothreadj/unittest/src/main.c
@@ -16,6 +16,7 @@ check_config(void)
 {
 	//---Definitions---//
 	static const char*	cc_name		= COTHREAD_CC_ID_GCC			== COTHREAD_CC_ID ? "gcc"
+									: COTHREAD_CC_ID_CLANG			== COTHREAD_CC_ID ? "clang"
 									: "???";
 
 	static const char*	arch_name	= COTHREAD_ARCH_ID_X86			== COTHREAD_ARCH_ID ? "x86"
@@ -23,6 +24,7 @@ check_config(void)
 									: "???";
 
 	static const char*	os_name		= COTHREAD_OS_ID_GNU_LINUX		== COTHREAD_OS_ID ? "gnu_linux"
+									: COTHREAD_OS_ID_FREEBSD		== COTHREAD_OS_ID ? "freebsd"
 									: "???";
 
 	//---Check the configuration---//
